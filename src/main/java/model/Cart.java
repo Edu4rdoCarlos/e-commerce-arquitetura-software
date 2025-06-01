6ceb77f98d8170a74c36869eecdde86e2ec6d8cc
Eduardo Carlos <edu4rdobarb0za@gmail.com>
2025-06-01 00:33:09 -0300

    feat: add models and db connection

---
+++ src/main/java/model/Cart.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.List;
+
+public class Cart {
+    private Long id;
+    private User user;
+    private List<Product> products;
+
+    public Cart(Long id, User user, List<Product> products) {
+        this.id = id;
+        this.user = user;
+        this.products = products;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public List<Product> getProducts() { return products; }
+    public void setProducts(List<Product> products) { this.products = products; }
+}
